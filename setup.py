dc7a6243e3849b6045a04b2d0f5746464e47786c
ambro034 <123208997+ambro034@users.noreply.github.com>
2024-06-10 14:52:05 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name = 'text_reuse',
+  version = '0.0.1',
+  description = "A package used to identify reused test between two portions of text",
+  url = 'https://github.com/ambro034/text_reuse.git',
+  author_email = '',
+  license = 'unlicense',
+  package = ['text_reuse'],
+  zip_safe = False
+)
+  
